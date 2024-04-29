6f5b7d1763caebed9243baedf4af0c0ccff65bbb
Piyush Jha <95270392+prykton@users.noreply.github.com>
2024-04-29 10:47:37 +0530

    Update DiagonalSum.java

--- 2D arrays/DiagonalSum.java
+++ 2D arrays/DiagonalSum.java
@@ -1,6 +1,20 @@
 import java.util.*;
 public class DiagonalSum {
 
+    /*
+    BrutForce Method
+    for(int i = 0; i < matrix.length; i++) {
+        for(int j = 0; j < matrix[0].length; j++) {
+            if(i == j)
+                sum += matrix[i][j];
+
+            else if(i+j == matrix.length-1)
+                sum += matrix[i][j];
+        }
+    }
+    return sum;
+    */
+
     public static int Sum(int array[][]) {
         int sum = 0;
         for(int i = 0; i < array.length; i++) {
