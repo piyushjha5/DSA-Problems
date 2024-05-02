da78203df38d8b7c31fd6d0f9038e42aead14a0a
Piyush Jha <95270392+prykton@users.noreply.github.com>
2024-05-02 23:15:53 +0530

    Update StringCompression.java

--- string/StringCompression.java
+++ string/StringCompression.java
@@ -2,19 +2,19 @@ import java.util.*;
 public class StringCompression {
 
     public static String Compress(String str) {
-        String newstr = "";
+        StringBuilder newstr = new StringBuilder("");
         for(int i = 0; i < str.length(); i++) {
             Integer count = 1;
             while(i < str.length()-1 && str.charAt(i) == str.charAt(i+1)) {
                 count++;
                 i++;
             }
-            newstr += str.charAt(i);
+            newstr.append(str.charAt(i));
             if(count > 1) {
-                newstr += count.toString();
+                newstr.append(count);
             }
         }
-        return newstr;
+        return newstr.toString();
     }
 
     public static void main(String args[]) {
